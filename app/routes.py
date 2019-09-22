7d6a56b527cea509f240939b743b7473d67f3e1b
Rohit <rohit@Rohits-MacBook-Pro-2.local>
2019-09-22 16:59:51 +0000

    Bare bones framework for website

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,26 @@
 from app import app
-from flask import escape, redirect, url_for
+from flask import escape, redirect, url_for, render_template
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/signup')
+def tutor_signup():
+    return render_template('signup.html')
+
+@app.route('/request')
+def tutor_request():
+    return render_template('request.html')
+
+@app.route('/teacher_student_request')
+def teacher_student_request():
+    return render_template('teacher_student_request.html')
+
+@app.route('/teacher_admin')
+def teacher_admin():
+    return render_template('teacher_admin.html')
 
 # @app.route('/<string:user>')
 # def user(user):
@@ -16,17 +33,17 @@ def hello():
 # def say(numb):
 #     return 'You are number %d' % numb
 
-@app.route('/student/<string:student>')
-def hello_student(student):
-    return 'Hello student %s!' % escape(student)
+# @app.route('/student/<string:student>')
+# def hello_student(student):
+#     return 'Hello student %s!' % escape(student)
 
-@app.route('/teacher/<string:teacher>')
-def hello_teacher(teacher):
-    return 'Hello teacher %s' % escape(teacher)
+# @app.route('/teacher/<string:teacher>')
+# def hello_teacher(teacher):
+#     return 'Hello teacher %s' % escape(teacher)
 
-@app.route('/user/<string:name>')
-def hello_u(name):
-    if name == 'rohit':
-        return redirect(url_for('hello_teacher', teacher = name))
-    else:
-        return redirect(url_for('hello_student', student = name))
+# @app.route('/user/<string:name>')
+# def hello_u(name):
+#     if name == 'rohit':
+#         return redirect(url_for('hello_teacher', teacher = name))
+#     else:
+#         return redirect(url_for('hello_student', student = name))
